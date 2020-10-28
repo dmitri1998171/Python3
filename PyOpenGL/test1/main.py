772edf2a3cc724d27d970b64e98c96f3c019aff4
Dmitry <dmitri1998171@icloud.com>
2020-10-28 13:46:48 +0700

    add FPS

--- PyOpenGL/test1/main.py
+++ PyOpenGL/test1/main.py
@@ -22,6 +22,12 @@ deltaAngle, deltaMove = 0, 0
 xOrigin = -1
 yOrigin = -1
 
+font = GLUT_STROKE_ROMAN
+W,H = 800,600
+frame = 0
+time, timebase = 0,0
+fps = 0
+
 def reshape(width, height):
     glViewport(0, 0, width, height)
     glMatrixMode(GL_PROJECTION)
@@ -138,22 +144,49 @@ def computePos(deltaMove):
     camX = camX + deltaMove * lx * 0.1
     camZ = camZ + deltaMove * lz * 0.1
 
-# def computeDir(deltaAngle):
-#     global lx,lz,angle
-#     angle = angle + deltaAngle
-#     lx = math.sin(angle)
-#     lz = math.cos(angle)
+def blit_text(x,y,scale,w,font,text,r,g,b):
+    glMatrixMode(GL_PROJECTION)
+    glPushMatrix()
+    glLoadIdentity()
+
+    glOrtho(0, W, 0, H, -1, 1)
+
+    glMatrixMode(GL_MODELVIEW)
+    glPushMatrix()
+    glLoadIdentity()
+    
+    glColor3f(r, g, b)
+    glLineWidth(w)
+    glTranslatef(x, y, 0)
+    glScalef(scale, scale, scale)
+    for ch in text:
+        glutStrokeCharacter(font,ctypes.c_int(ord(ch)))
+    
+    glMatrixMode(GL_PROJECTION)
+    glPopMatrix()
+
+    glMatrixMode(GL_MODELVIEW)
+    glPopMatrix()
+
+def fpsFunc():
+    global fps, frame, time, timebase
+    frame = frame + 1
+    time=glutGet(GLUT_ELAPSED_TIME)
+    if (time - timebase > 1000):
+        fps = frame*1000.0/(time-timebase)
+        timebase = time
+        frame = 0
+    return fps
 
 def draw():
     global camX,camY,camZ,lx,lz,angle,deltaAngle,deltaMove
+    
     if (deltaMove): 
         computePos(deltaMove)
-    # if (deltaAngle): 
-    #     computeDir(deltaAngle)
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
-    print("tx: ", camX+lx, "\ttz: ", camZ+lz,"\tangle: ", angle)
+    # print("tx: ", camX+lx, "\ttz: ", camZ+lz,"\tangle: ", angle)
     gluLookAt(camX,    camY, camZ, 
               camX+lx, camY+ly, camZ+lz,
               0.0,     1, 0.0)
@@ -165,9 +198,15 @@ def draw():
         for j in range(10):
             Cube(i,0,j)
     
-    # glutSolidCube(1, 1, 1)
-    glutSwapBuffers()
+    text_fps = "FPS: " + str(int(fpsFunc()))
+    blit_text(W-(len(text_fps)*17),H-30, 0.2, 3, 
+        GLUT_STROKE_ROMAN, text_fps, 0, 255, 0)
 
+    # print(text_fps)
+    print("deltaMove: ", deltaMove)
+
+    glutSwapBuffers()
+# =================================================
 def lightFunc():
     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient) # Определяем текущую модель освещения
     glEnable(GL_LIGHTING)                           # Включаем освещение
@@ -180,45 +219,33 @@ def textureInit():
     glEnable     ( GL_TEXTURE_2D )
     glHint       ( GL_POLYGON_SMOOTH_HINT,         GL_NICEST )
     glHint       ( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST )
-
+# =================================================
 def keyboard(key, foo, bar):
-    global camX, camY, camZ, deltaMove, deltaAngle 
+    global deltaMove
     if key == ESCAPE: os.kill(0,4)
 
-    # if key == b'd': deltaAngle = 0.01
-    # if key == b'a': deltaAngle = -0.01
-    if key == b's': deltaMove = -0.5
-    if key == b'w': deltaMove = 0.5
-    glutPostRedisplay()
-
 def specialkeys(key, x, y):
-    global xrot,yrot
-    if key == GLUT_KEY_UP: xrot -= 2.0             
-    if key == GLUT_KEY_DOWN: xrot += 2.0             
-    if key == GLUT_KEY_LEFT: yrot -= 2.0             
-    if key == GLUT_KEY_RIGHT: yrot += 2.0             
-    glutPostRedisplay()       
+    global deltaMove
+    if key == GLUT_KEY_UP: deltaMove = 0.5             
+    if key == GLUT_KEY_DOWN: deltaMove = -0.5
 
 def releaseKey(key, x, y):
-    global deltaAngle, deltaMove
-    # if key == b'd' or key == b'a':  deltaAngle = 0
-    # if key == b'w' or key == b's':  deltaMove = 0
-    
-    if key == b'w': deltaMove = 0
-    if key == b's': deltaMove = 0
-
+    global deltaMove
+    if ((key == GLUT_KEY_UP) or (key == GLUT_KEY_DOWN)):
+        deltaMove = 0
+# =================================================
 def mouseButton(button, state, x, y):
     global xOrigin, yOrigin, angle
     if (button == GLUT_LEFT_BUTTON):
-        if (state == GLUT_UP):
+        if (state == GLUT_DOWN):
             angle += deltaAngle
             xOrigin = -1
-            yOrigin = -1
+            yOrigin = 1
         else:
             xOrigin = x
             yOrigin = y
 
-def mouseMove(x, y):
+def passiveMouseMove(x, y):
     global xOrigin, yOrigin, angle, deltaAngle, lx, ly, lz
     if (xOrigin >= 0):
         deltaAngle = -(x - xOrigin) * 0.01
@@ -227,28 +254,33 @@ def mouseMove(x, y):
     if (yOrigin >= 0):
         deltaAngle = -(y - yOrigin) * 0.01
         ly = math.sin(angle + deltaAngle)
-
+# =================================================
 def init():
     glClearColor(0.5, 0.5, 0.5, 1.0)                # Серый цвет для первоначальной закраски
     glEnable(GL_DEPTH_TEST)
+    glEnable(GL_CULL_FACE)
     glutSetCursor(GLUT_CURSOR_NONE)
     lightFunc()
     textureInit()
 
 glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
-glutInitWindowSize(800, 600)
+glutInitWindowSize(W, H)
 glutInitWindowPosition(300, 150)
 glutInit(sys.argv)
 glutCreateWindow(b"PyCraft")
 
 glutDisplayFunc(draw)
 glutReshapeFunc(reshape)
+glutIdleFunc(draw) 
+
+glutIgnoreKeyRepeat(1)
+
 glutKeyboardFunc(keyboard)
 glutSpecialFunc(specialkeys)
-glutMouseFunc(mouseButton)
-glutMotionFunc(mouseMove)
-# glutIgnoreKeyRepeat(1)
 glutSpecialUpFunc(releaseKey)
+
+glutMouseFunc(mouseButton)
+glutPassiveMotionFunc(passiveMouseMove)
 
 init()
 
